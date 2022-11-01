c095c1d4d066f96f85938eef83b273106cc4fbae
Marcin Kolenda <marcinkolenda419@gmail.com>
2022-11-01 10:31:00 +0100

    wrap zip header reader into class and move to new ns

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System.Text;
 using System.Runtime.InteropServices;
+using ReadFileHeader.Reader;
 
 namespace ReadFileHeader
 {
@@ -8,35 +9,9 @@ namespace ReadFileHeader
         static unsafe void Main(string[] args)
         {
             FileStream file = File.OpenRead("sample.zip");
-            BinaryReader reader = new BinaryReader(file);
+            var header = ZIPHeaderReaderMarshal.Read(file);
 
-            int size = Marshal.SizeOf<ZIPHeader>();
-            ReadOnlySpan<byte> span = new ReadOnlySpan<byte>(reader.ReadBytes(size));
-            ZIPHeader header;
-            bool succeed = MemoryMarshal.TryRead<ZIPHeader>(span, out header);
-
-            Console.WriteLine($"Size of uncompressed file: {header.uncompressedFileSize} bytes");
+            Console.WriteLine($"Size of uncompressed file: {header?.uncompressedFileSize} bytes");
         }
-    }
-
-    /// <summary>
-    /// Header of basic PKZip file (without extensions).
-    /// <see cref="https://www.fileformat.info/format/zip/corion.htm"/>
-    /// <see cref="https://users.cs.jmu.edu/buchhofp/forensics/formats/pkzip.html"/>
-    /// </summary>
-    [StructLayout(LayoutKind.Sequential, Pack = 2)]
-    public unsafe struct ZIPHeader
-    {
-        public fixed byte id[4];
-        public ushort version;
-        public short generalPurposeBit;
-        public short compressionMethod;
-        public ushort modTime;
-        public ushort modDate;
-        public int crc1;
-        public uint compressedFileSize;
-        public uint uncompressedFileSize;
-        public short lengthOfFilename;
-        public short lengthOfExtraField;
     }
 }
---
+++ Reader/ZIPHeaderReaderMarshal.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReadFileHeader.Reader;
+public class ZIPHeaderReaderMarshal
+{
+    public static ZIPHeader? Read(FileStream fileStream)
+    {
+        BinaryReader reader = new BinaryReader(fileStream);
+
+        int size = Marshal.SizeOf<ZIPHeader>();
+        ReadOnlySpan<byte> span = new ReadOnlySpan<byte>(reader.ReadBytes(size));
+        ZIPHeader header;
+        bool succeed = MemoryMarshal.TryRead<ZIPHeader>(span, out header);
+        return succeed ? header : null;
+    }
+
+    /// <summary>
+    /// Header of basic PKZip file (without extensions).
+    /// <see cref="https://www.fileformat.info/format/zip/corion.htm"/>
+    /// <see cref="https://users.cs.jmu.edu/buchhofp/forensics/formats/pkzip.html"/>
+    /// </summary>
+    [StructLayout(LayoutKind.Sequential, Pack = 2)]
+    public unsafe struct ZIPHeader
+    {
+        public fixed byte id[4];
+        public ushort version;
+        public short generalPurposeBit;
+        public short compressionMethod;
+        public ushort modTime;
+        public ushort modDate;
+        public int crc1;
+        public uint compressedFileSize;
+        public uint uncompressedFileSize;
+        public short lengthOfFilename;
+        public short lengthOfExtraField;
+    }
+}
+
